417222b31ffc471919de231a3026010cd7ce7207
Melva Lang <mjlang74@gmail.com>
2019-12-11 19:28:31 -0600

    sixes and sevens

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -6,11 +6,19 @@ export class Satellite {
     orbitType: string;
     operational: boolean;
 
+    
     constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
-    this.name = name;
-    this.type = type;
-    this.launchDate = launchDate;
-    this.orbitType = orbitType;
-    this.operational = operational;
+        this.name = name;
+        this.type = type;
+        this.launchDate = launchDate;
+        this.orbitType = orbitType;
+        this.operational = operational;
     };
+    shouldShowWarning(){
+        if(this.type.toLowerCase() === 'space debris'){
+            return true;
+        }else{
+            return false;
+        }
+    }
 };
